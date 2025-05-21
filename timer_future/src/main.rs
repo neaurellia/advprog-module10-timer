5e4b8be766343c897f3b9eb1b48ab6da48b1bca7
neaurellia <catherineaurellia@gmail.com>
2025-05-21 23:46:43 +0700

    Experiment 1.2: Understanding how it works.

--- timer_future/src/main.rs
+++ timer_future/src/main.rs
@@ -99,12 +99,13 @@ fn main() {
 
     // Spawn a task to print before and after waiting on a timer.
     spawner.spawn(async {
-        println!("howdy!");
+        println!("Catherine's Komputer: howdy!");
         // Wait for our timer future to complete after two seconds.
         TimerFuture::new(Duration::new(2, 0)).await;
-        println!("done!");
+        println!("Catherine's Komputer: done!");
     });
 
+    println!("Catherine's Komputer: hey hey");
     // Drop the spawner so that our executor knows it is finished and won't
     // receive more incoming tasks to run.
     drop(spawner);
